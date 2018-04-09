20743127fad7d1f895a38191195833618c4d396d
kyriosdata <fabio@inf.ufg.br>
2018-04-09 15:55:18 -0300

    melhorias

---
+++ topico1/src/test/java/br/ufg/inf/es/integracao/Atividade1Test.java
@@ -0,0 +1,12 @@
+package br.ufg.inf.es.integracao;
+
+import br.ufg.inf.es.integracao.atividade1.Atividade1;
+import org.junit.Test;
+
+public class Atividade1Test {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoInexistenteGeraExcecao() throws Exception {
+        Atividade1.exibirHexa("");
+    }
+}
