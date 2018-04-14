a22cef449efbe378f924aa10b65da4ce6ba3f3ac
Antonio Silva <antlis@ufg.br>
2018-04-14 20:06:13 -0300

    Create Atividade6Test.java

---
+++ topico1/src/test/java/br/ufg/inf/es/integracao/Atividade6Test.java
@@ -0,0 +1,23 @@
+package br.ufg.inf.es.integracao;
+
+import br.ufg.inf.es.integracao.atividade6.Atividade6;
+import org.junit.Test;
+
+public class Atividade6Test {
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoInexistenteGeraExcecao() throws Exception {
+        Atividade6.viewToTxt("");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoNomeEmBrancoGeraExcecao() throws Exception {
+        Atividade6.viewToTxt(" ");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoSemNomeGeraExcecao() throws Exception {
+        Atividade6.viewToTxt();
+    }
+    
+}
