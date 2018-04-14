b87475b1c18f83d896b32a21d8b0504e55a5d6bd
Antonio Silva <antlis@ufg.br>
2018-04-14 19:53:21 -0300

    Create Atividade3Test.java

---
+++ topico1/src/test/java/br/ufg/inf/es/integracao/Atividade3Test.java
@@ -0,0 +1,23 @@
+package br.ufg.inf.es.integracao;
+
+import br.ufg.inf.es.integracao.atividade3.Atividade3;
+import org.junit.Test;
+
+public class Atividade3Test {
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoInexistenteGeraExcecao() throws Exception {
+        Atividade3.exibirArq("");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoNomeEmBranco() throws Exception {
+        Atividade3.exibirArq(" ");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoSemNome() throws Exception {
+        Atividade3.exibirArq();
+    }
+    
+}
