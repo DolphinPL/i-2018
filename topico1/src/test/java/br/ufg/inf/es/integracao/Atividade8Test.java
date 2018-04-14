d08eb53068304cb8c1be4c13cbd7a981d8f8d711
Antonio Silva <antlis@ufg.br>
2018-04-14 20:10:09 -0300

    Create Atividade8Test.java

---
+++ topico1/src/test/java/br/ufg/inf/es/integracao/Atividade8Test.java
@@ -0,0 +1,23 @@
+package br.ufg.inf.es.integracao;
+
+import br.ufg.inf.es.integracao.atividade8.Atividade8;
+import org.junit.Test;
+
+public class Atividade8Test {
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoOneInexistenteGeraExcecao() throws Exception {
+        Atividade8.viewToTxt("", 2);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoNomeEmBrancoOneGeraExcecao() throws Exception {
+        Atividade8.viewToTxt(" ", 2);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void arquivoSemNomeOneGeraExcecao() throws Exception {
+        Atividade8.viewToTxt(, 2);
+    }
+  
+}
