cd16a839c50a4596ab686c7a0e8a58a40263f11d
antoni-s <antlis@ufg.br>
2018-05-13 17:04:04 -0300

    correções topico 2

--- topico2/src/main/java/br/ufg/inf/es/integracao/serializacao/atividade2/SerializaDeserializa.java
+++ topico2/src/main/java/br/ufg/inf/es/integracao/serializacao/atividade2/SerializaDeserializa.java
@@ -58,16 +58,30 @@ public class SerializaDeserializa {
      */
     public static Funcionario deserializar(String nomeArquivo) throws IOException, ClassNotFoundException {
 
-        if (nomeArquivo.equals(null) || nomeArquivo.trim().isEmpty()) {
+        if (!nomeValido(nomeArquivo)) {
             throw new IllegalArgumentException("Ops, nome de arquivo é invalido!");
+        } else {
+
+            FileInputStream inFile = new FileInputStream(nomeArquivo);
+            ObjectInputStream d = new ObjectInputStream(inFile);
+            Funcionario objeto = (Funcionario) d.readObject();
+
+            d.close();
+            return objeto;
+        }
+    }
+
+    /**
+     * Método que verifica se o nome de um arquivo é valido.
+     * @param file Nome do arquivo.
+     * @return True nome valido.
+     */
+    private static boolean nomeValido(String file) {
+        if (file.equals(null) || file.trim().isEmpty()) {
+            return false;
         }
 
-        FileInputStream inFile = new FileInputStream(nomeArquivo);
-        ObjectInputStream d = new ObjectInputStream(inFile);
-        Funcionario objeto = (Funcionario)d.readObject();
-
-        d.close();
-        return objeto;
+        return true;
     }
 
 }
